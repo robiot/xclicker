fe4b94c17fc8c8a53d81533f99ca05dbf5737a53
robiot <robiot@users.noreply.github.com>
2021-10-06 00:29:38 +0300

    Ui update

--- src/mainwin.c
+++ src/mainwin.c
@@ -10,21 +10,31 @@ struct _MainAppWindow
 
 G_DEFINE_TYPE(MainAppWindow, main_app_window, GTK_TYPE_APPLICATION_WINDOW);
 
-static void main_app_window_init (MainAppWindow *win)
+// Handlers
+void insert_handler(GtkEditable *editable, const gchar *text, gint length, gint *position, gpointer user_data)
 {
-  gtk_widget_init_template (GTK_WIDGET (win));
+  if (!g_unichar_isdigit(g_utf8_get_char(text)))
+  {
+    g_signal_stop_emission_by_name(editable, "insert_text");
+  }
 }
 
-static void main_app_window_class_init (MainAppWindowClass *class)
+static void main_app_window_init(MainAppWindow *win)
+{
+  gtk_widget_init_template(GTK_WIDGET(win));
+}
+
+static void main_app_window_class_init(MainAppWindowClass *class)
 {
   gtk_widget_class_set_template_from_resource(GTK_WIDGET_CLASS(class), "/org/gtk/xclicker/ui/xclicker-window.ui");
+  gtk_widget_class_bind_template_callback(GTK_WIDGET_CLASS(class), insert_handler);
 }
 
-MainAppWindow *main_app_window_new (XClickerApp *app)
+MainAppWindow *main_app_window_new(XClickerApp *app)
 {
-  return g_object_new (MAIN_APP_WINDOW_TYPE, "application", app, NULL);
+  return g_object_new(MAIN_APP_WINDOW_TYPE, "application", app, NULL);
 }
 
-void main_app_window_open (MainAppWindow *win, GFile *file)
+void main_app_window_open(MainAppWindow *win, GFile *file)
 {
 }
