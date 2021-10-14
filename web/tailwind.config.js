e58587defb0c9cb3fec71461fbfc958977cded20
robiot <robiot@users.noreply.github.com>
2021-10-14 22:10:06 +0300

    Fix

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,19 +1,22 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: [],
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         "gradient-gray": "#1e3135",
         "gradient-gray-less": "#3d5062",
-        gray: colors.trueGray,
+        "gray": colors.trueGray,
       },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [require("tailwindcss-container-sizes")()],
+
+  plugins: [
+    require('tailwindcss-container-sizes')(),
+  ],
 };
