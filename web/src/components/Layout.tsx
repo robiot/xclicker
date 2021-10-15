cf0bdb6ad63b7878875387dd30d1dff6310371cf
robiot <robiot@users.noreply.github.com>
2021-10-15 15:46:18 +0300

    web: Add embed

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -8,6 +8,10 @@ export function Layout({ children, title, home = false }) {
         <title>{home ? `XClicker - ${title}` : `${title} - XClicker`}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="Description" content="A fast gui autoclicker for linux" />
+        <meta content="https://xclicker.pages.dev" property="og:url" />
+        <meta content="/embed.png" property="og:image" />
+        <meta content="#43B581" data-react-helmet="true" name="theme-color" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
