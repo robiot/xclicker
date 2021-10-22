64f2bd7ea46f75aa12b0c502a2a32f426fc3de8e
robiot <robiot@users.noreply.github.com>
2021-10-22 22:49:00 +0300

    Remove unnecessary code

--- src/xclicker-app.c
+++ src/xclicker-app.c
@@ -21,28 +21,10 @@ static void xclicker_app_activate(GApplication *app)
 	gtk_window_present(GTK_WINDOW(win));
 }
 
-static void xclicker_app_open(GApplication *app, GFile **files, int n_files, const char *UNUSED(sus))
-{
-	GList *windows;
-	MainAppWindow *win;
-	int i;
-
-	windows = gtk_application_get_windows(GTK_APPLICATION(app));
-	if (windows)
-		win = XCLICKER_APP_WINDOW(windows->data);
-	else
-		win = main_app_window_new(XCLICKER_APP(app));
-
-	for (i = 0; i < n_files; i++)
-		main_app_window_open(win, files[i]);
-
-	gtk_window_present(GTK_WINDOW(win));
-}
 
 static void xclicker_app_class_init(XClickerAppClass *class)
 {
 	G_APPLICATION_CLASS(class)->activate = xclicker_app_activate;
-	G_APPLICATION_CLASS(class)->open = xclicker_app_open;
 }
 
 XClickerApp *xclicker_app_new(void)
