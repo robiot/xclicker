2afa320940745cd3bb8f7518260345f4559f5078
Robiot <r0w80abc@gmail.com>
2022-04-27 16:59:31 +0300

    Format code

--- src/settings.h
+++ src/settings.h
@@ -7,17 +7,17 @@ extern gboolean isChoosingHotkey;
 
 /**
  * Checks if configured to use safe-mode.
-*/
+ */
 gboolean is_safemode();
 
 /**
  * Checks if configured to use xevent.
-*/
+ */
 gboolean is_using_xevent();
 
 /**
  * Loads config file if exists, else create it.
-*/
+ */
 void config_init();
 
 void load_start_stop_keybinds();
